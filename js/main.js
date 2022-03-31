4a07edd8c158a4e39ab05ab16a94808fadc7ca6d
4atak <arshlmv@gmail.com>
2022-03-31 12:51:17 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,104 @@
+////////////////////// // 1 task //////////////////////////////////
+//! 1
+// function checkTask(){   
+//     let newArr = arr.filter(item=>item>0)
+//     console.log(newArr)
+// }
+// checkTask([2, 3, 5, 9, 8, -11, 6, 7, 47, 53, -1, -3, 17]) 
+
+//! 2
+// function checkTask(arr){
+//     let sum = 0;
+//     arr.forEach(i=>{
+//         sum=sum+i
+//     })
+//         console.log(sum);
+// }
+// checkTask([10, 25, 25, 15, 5, 10])
+
+
+
+//! 3
+
+// function checkTask(){
+//     let arr = [175, 15, 25];
+//     let newArr = arr.reduce((a, b) => a - b)
+//     console.log(newArr)
+//     }
+//     checkTask();
+
+
+
+////////////////////// // 4 task //////////////////////////////////
+
+
+
+
+////////////////////// // 5 task //////////////////////////////////
+
+
+
+
+
+////////////////////// // 6 task //////////////////////////////////
+
+
+
+
+
+
+//! 7 task
+// function checkTask(arr){
+//     let res=arr.every((item)=>item>0)
+//     console.log(res)
+// }
+// checkTask([4, 5, 8, 9]);
+
+
+
+
+
+//!8
+// function checkTask(arr){
+//     //let arr = ['июнь', 'октябрь', 'май', 'ноябрь', 'март'] ;
+//     let res = arr.filter(item=>item.length>5)
+//     console.log(res)
+// }
+// checkTask(['июнь', 'октябрь', 'май', 'ноябрь', 'март'])
+
+
+
+//!9
+// let arr = [1, 2, 3, 0, 4, 5, 6] 
+// function checkTask(arr){
+//     let arr2;
+//     arr.reduce((a,b)=>{
+//         if(b===0){
+//             return arr2 = a
+//         }else{
+//             return a + b
+//         }
+//     })
+//     if(typeof(arr2)=== 'undefined'){
+//         arr2 = 0
+//     }
+//     return console.log(arr2)
+// }
+// checkTask(arr)
+
+//! 10
+// let arr = ['Это заголовок <h1>', 'А это параграф <p>', 'А это тег <div>'];
+// let checkTask = (arr) => {
+//   arr.forEach((i) => {
+//     if (i.includes('<h1>')) {
+//       console.log(`<h1>${i}</h1>`);
+//     } else if (i.includes('<p>')) {
+//       console.log(`<p>${i}</p>`);
+//     } else if (i.includes('<div>')) {
+//       console.log(`<div>${i}</div>`);
+//     }
+//   });
+// };
+// checkTask(arr);
+
+
